4a5f938893b38f5ab8902411f667a65a77cd3922
jalvarenga11 <jealvarengar@unah.edu.hn>
2025-05-26 18:04:33 -0600

    desestructuracion de listas y objetos

--- intro-node/02-estructuras.js
+++ intro-node/02-estructuras.js
@@ -1,8 +1,11 @@
 // listas (array) y mapas / objetos / diccionarios
 
 const numeros = [1, 2, 3, 4, 5]
+const copia = [...numeros] // operador spread
 numeros.splice(2, 1) // elimina el tercer elemento
 numeros.push(6) // añade un elemento al final
+numeros.push(61) // añade un elemento al final
+numeros.push(16) // añade un elemento al final
 numeros.unshift(0) // añade un elemento al principio
 numeros.pop() // elimina el ultimo elemento
 numeros.shift() // elimina el primer elemento
@@ -12,6 +15,7 @@ numeros[0] // 1
 
 // numeros = true
 console.log(numeros)
+console.log(copia)
 
 // mapas
 
@@ -45,3 +49,56 @@ console.log(mascota.nombre)
 console.log(mascota['comida favorita'])
 console.log(mascota['raza'])
 console.log(mascota)
+
+// Desestructuración (...) // spread / rest
+
+const [primer, segundo, tercer, ...restantes] = copia
+// const primero = copia[0]
+// const segundo = copia[1]
+// const tercero = copia[2]
+
+console.log(primer)
+console.log(segundo)
+console.log(tercer)
+console.log(restantes)
+console.log(copia)
+
+//Desestructuración de objetos
+
+const nombre = 'Juan Perez'
+
+const copiaMascota = { ...mascota }
+
+const { vacunas: vacunasAplicadas, nombre: nombreMascota, 'comida favorita': comidaFavoria, gato, apodo } = mascota
+// const nombre = mascota.nombre
+// const apodo = mascota.apodo
+
+console.log(nombre)
+console.log(apodo)
+console.log(comidaFavoria)
+console.log(vacunasAplicadas)
+
+const persona = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    edad: 30,
+    direccion: {
+        calle: 'Falsa',
+        numero: 123,
+        ciudad: 'Springfield',
+        geo: {
+            lat: -34.6037,
+            lng: -58.3816
+        }
+    }
+}
+
+// const { direccion: { ciudad, geo: { lat, lng } } } = persona
+// ⬆️ esta instrucción es equivalente a las siguientes: ⬇️
+const { direccion } = persona
+const { ciudad, geo } = direccion
+const { lat, lng } = geo
+
+console.log(ciudad)
+console.log(lat)
+console.log(lng)
