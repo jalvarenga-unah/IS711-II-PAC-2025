ae0e725629b9d9318f12162f49c8c9b70fdf597d
jalvarenga11 <jealvarengar@unah.edu.hn>
2025-05-28 17:48:39 -0600

    manejo de funciones y desestrucutracion

--- intro-node/04-funciones.js
+++ intro-node/04-funciones.js
@@ -17,4 +17,47 @@ console.log(saludo);
 saludo()
 saludo2()
 
+// funci;on de flecha
+const saludo3 = (nombre) => {
 
+    // no tiene acceso a "this" en su lugar
+    // use "globalThis" 
+    // console.log('Hola' + nombre)
+    console.log(`Hola ${nombre ?? ''} `) // '' o ""
+
+}
+
+
+saludo3('Pedro')
+saludo3()
+
+
+// bool
+const isNumber = (value) => {
+
+    const resultado = Number(value)
+
+    // if (resultado >= 0 || resultado < 0) return true
+    // else return false
+
+    return !isNaN(resultado)
+}
+
+// función corta para retorna en una sola línea
+const isNumber2 = (value) => !isNaN(Number(value))
+
+console.log(isNumber(5))
+console.log(isNumber('5'))
+console.log(isNumber('5e'))
+console.log(isNumber(0))
+console.log(isNumber(-1))
+
+
+const crearPersona = ({ nombre, edad = 0 }) => {
+    console.log(`Creando persona ${nombre} de ${edad} años`)
+}
+
+const params = { edad: 31, nombre: 'Juan', apellido: 'Perez' }
+
+crearPersona({ nombre: 'Pedro' }) // argumentos "con nombre"
+crearPersona(params)
