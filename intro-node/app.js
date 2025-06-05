18e8b1ceb0d8162bff6e5d9653a71211d1c4e965
jalvarenga11 <juan.alvarenga@ess.hn>
2025-06-04 18:03:32 -0600

    introduccion a aplicacion web server con node

--- intro-node/app.js
+++ intro-node/app.js
@@ -1,1 +1,28 @@
-console.log('Mi primer servidor web')
+import { createServer } from 'node:http'
+
+// mi serviro web
+const server = createServer((req, res) => {
+
+    // ? para devolver una respuesta al cliente en texto plano/html
+    // res.end('<h1>Hola mundo desde Node.js!!!!</h1>') // estoy devolviendo una respuesta al cliente
+
+    // establecer información en el encabezado de la respuesta
+    res.writeHead(200, 'Content-Type: application/json')
+
+
+    const { url, method } = req // desestructuración de la petición 
+
+
+    //? se tranforma el formato a string
+    res.end(JSON.stringify({
+        success: true,
+        message: 'Hola mundo'
+    }))
+
+})
+
+//escuchando todas las peticiones
+server.listen(3000, () => {
+
+    console.log('Server is running on http://localhost:3000')
+})
