95d13dc2b77d3694ea740e855fec78943800e5c2
jalvarenga11 <juan.alvarenga@ess.hn>
2025-06-11 17:49:50 -0600

    manejo de middlewares, params y query params

--- movies-unah/index.js
+++ movies-unah/index.js
@@ -1,7 +1,26 @@
 import express from 'express'
+import movies from './local_db/movies.json' with { type: 'json' }
 
 const app = express() // para crear la aplicación de express
 const PORT = process.env.PORT || 3000 // puerto donde se ejecutará la aplicación
+
+// middlewares
+app.use(express.json()) // se encarga de parsear el body de las peticiones
+// app.use((req, res, next) => {
+
+//     let body = '';
+
+//     req.on('data', (chunk) => {
+//         body += chunk.toString(); // convierte el buffer a string
+//     });
+
+//     req.on('end', () => {
+//         req.body = JSON.parse(body); // convierte el string a JSON
+//         next()
+//     })
+
+//     console.log('Middleware ejecutado');
+// })
 
 // rutas
 
@@ -11,10 +30,25 @@ app.get('/', (req, res) => {
 
 app.get('/movies', (req, res) => {
     // devuelve en formato JSON una lista de películas en los encabezados
-    res.json({})
+    res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+
+    const { id } = req.params
+    const { query } = req
+
+    res.json({
+        id,
+        query
+    })
+
 })
 
 app.post('/movies', (req, res) => {
+
+    movies.push(req.body) // agrega la película al array de películas
+
     res
         .status(201) // establece el código de estado HTTP a 201 (Creado)
         .json(req.body)
